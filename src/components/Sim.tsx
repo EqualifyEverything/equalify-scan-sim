856c90ddeba5abca25245954853a2b4d7f386b2b
azdak <negatia@gmail.com>
2024-09-02 10:50:58 -0500

    Cleanup and refactor

--- src/components/Sim.tsx
+++ src/components/Sim.tsx
@@ -1,124 +1,69 @@
-import { Box, Button } from "@radix-ui/themes";
+import {
+  Box,
+  Button,
+  Container,
+  DataList,
+  Flex,
+  Heading,
+  Section,
+  Separator,
+  Slider,
+} from "@radix-ui/themes";
 import { useEffect, useState } from "react";
-
-interface ScanCustomer {
-  jobsCount: number;
-  arrivalTick: number;
-  finishedTick: number | null;
-}
+import scanSim from "../inc/sim";
 
 export default function Sim() {
   // inputs
-  const [scanTime, setScanTime] = useState(5); // time per scan (s)
-  const [workers, setWorkers] = useState(1);
-  const [customersCount, setCustomersCount] = useState(3);
+  const [scanTime, setScanTime] = useState([5]); // time per scan (s)
+  const [workers, setWorkers] = useState([1]);
+  const [customersCount, setCustomersCount] = useState([3]);
   const [averageCustomerRequestSize, setAverageCustomerRequestSize] =
-    useState(5000);
+    useState([5000]);
 
-  const [simRuns, setSimRuns] = useState(1);
+  const [simRuns, setSimRuns] = useState([1]);
 
-  // consts
-  let secsPerTick = 5; // seconds per tick (s)
-  const [currentTick, setCurrentTick] = useState(0);
-
-  // intermediates
-  let simDuration = (7 * 24 * 60 * 60) / secsPerTick; // seconds in a week/secsPerTick
-
-  const [customers, setCustomers] = useState<ScanCustomer[]>([]);
-
-  const [customerPointer, setCustomerPointer] = useState(0);
-
-  function advanceTick() {
-    for (let i = 0; i < workers; i++) {
-      // advance the pointer
-      if (customerPointer >= customers.length - 1) {
-        setCustomerPointer(0);
-      } else {
-        setCustomerPointer(customerPointer + 1);
-      }
-      if (
-        customers[customerPointer].arrivalTick &&
-        currentTick > customers[customerPointer].arrivalTick &&
-        customers[customerPointer].finishedTick == null
-      ) {
-        let newArr = [...customers];
-        if (customers[customerPointer].jobsCount > 0) {
-          newArr[customerPointer].jobsCount--; // finish a scan
-        } else {
-          // if done, set the finishedTick and remove from look
-          newArr[customerPointer].finishedTick = currentTick;
-        }
-        setCustomers(newArr)
-      }
-      setCurrentTick(currentTick+1);
-    }
+  async function run() {
+    const results = await scanSim();
+    console.log(results);
   }
-
-  function run() {
-    let start = performance.now();
-    console.log(`Sim duration: ${(simDuration * secsPerTick).toFixed(2)}s`);
-    // start a new run
-    for (let j = 0; j < simRuns; j++) {
-      const newCustomers = [];
-      for (let k = 0; k < customersCount; k++) {
-        newCustomers.push({
-          jobsCount: Math.round(randn_bm(0, averageCustomerRequestSize * 2, 1)),
-          arrivalTick: Math.round(Math.random() * simDuration),
-          finishedTick: null,
-        });
-      }
-      setCustomers(newCustomers);
-      console.log(customers);
-      
-      for (let i = 0; i < simDuration; i++) {
-        advanceTick();
-      };
-      console.log(customers);
-      analyseResults(); 
-    }
-    let delta = performance.now() - start;
-
-    console.log(`Finished, took: ${delta.toFixed(2)}ms`);
-  }
-
-  const analyseResults = () => {
-    // TODO generate report
-    /*
-       - multiply values by secsPerTick to convert to seconds!
-       - calculate average job completion time (in minutes)
-       - generate jobs graph (simple array from sampling?)
-       - generate customer timeline 
-       - push to results array
-      */
-    return null;
-  };
 
   return (
-    <>
-      <Box>
-        <Button onClick={run}>Run</Button>
-      </Box>
-    </>
+    <Box>
+      <Container size="1" p="3">
+        <Box pb="6"> 
+            <Heading size={"2"} align={"left"}>Settings</Heading>
+            <Separator style={{ width:"100%", marginTop:"1em", marginBottom:".5em" }}/>
+            <Flex minWidth="300px" align={"center"} gap="4" pt="2">
+                <Box width="120px" style={{fontSize:10, lineHeight: 1.2, textAlign: "left", color: "#c8c8c8"}}>Avg Scan Time (s)</Box>
+                <Slider min={0} max={50} value={scanTime} onValueChange={setScanTime} disabled></Slider>
+                {scanTime}
+            </Flex>
+            <Flex minWidth="300px" align={"center"} gap="4" pt="2">
+                <Box width="120px" style={{fontSize:10, lineHeight: 1.2, textAlign: "left"}}>Workers</Box>
+                <Slider min={0} max={50} value={workers} onValueChange={setWorkers}></Slider>
+                {workers}
+            </Flex>
+            <Flex minWidth="300px" align={"center"} gap="4" pt="2">
+                <Box width="120px" style={{fontSize:10, lineHeight: 1.2, textAlign: "left"}}>Customers</Box>
+                <Slider min={0} max={50} value={customersCount} onValueChange={setCustomersCount}></Slider>
+                {customersCount}
+            </Flex>
+            <Flex minWidth="300px" align={"center"} gap="4" pt="2">
+                <Box width="120px" style={{fontSize:10, lineHeight: 1.2, textAlign: "left"}}>Avg Customer Request Size</Box>
+                <Slider min={0} max={20000} value={averageCustomerRequestSize} onValueChange={setAverageCustomerRequestSize}></Slider>
+                {averageCustomerRequestSize}
+            </Flex>
+            <Separator style={{ width:"100%", marginTop:"1em", marginBottom:"1em" }}/>
+            <Flex minWidth="300px" align={"center"} gap="4" pt="2">
+                <Box width="120px" style={{fontSize:10, lineHeight: 1.2, textAlign: "left"}}># of Simulation Runs</Box>
+                <Slider min={0} max={20000} value={simRuns} onValueChange={setSimRuns}></Slider>
+                {simRuns}
+            </Flex>
+        </Box>
+        <Box>
+          <Button onClick={run}>Run</Button>
+        </Box>
+      </Container>
+    </Box>
   );
-}
-
-// Exponential random number generator
-function randomExponential(lambda: number) {
-  return -Math.log(1 - Math.random()) / lambda;
-}
-
-// Standard Normal variate using Box-Muller transform.
-function randn_bm(min: number, max: number, skew: number) {
-  let u = 0,
-    v = 0;
-  while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-  while (v === 0) v = Math.random();
-  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-
-  num = num / 10.0 + 0.5; // Translate to 0 -> 1
-  if (num > 1 || num < 0) num = randn_bm(min, max, skew); // resample between 0 and 1 if out of range
-  num = Math.pow(num, skew); // Skew
-  num *= max - min; // Stretch to fill range
-  num += min; // offset to min
-  return num;
 }
---
+++ src/inc/sim.ts
@@ -0,0 +1,114 @@
+export default function scanSim() {
+  // inputs
+  let scanTime = 5; // time per scan (s)
+  let workers = 1;
+  let customersCount = 3;
+  let averageCustomerRequestSize = 5000;
+
+  let simRuns = 1;
+
+  // consts
+  let secsPerTick = 5; // seconds per tick (s)
+  let currentTick = 0;
+
+  // intermediates
+  let simDuration = (7 * 24 * 60 * 60) / secsPerTick; // seconds in a week/secsPerTick
+
+  interface ScanCustomer {
+    jobsCount: number;
+    arrivalTick: number;
+    finishedTick: number | null;
+  }
+
+  const customers: ScanCustomer[] = [];
+
+  const provisionCustomers = () => {
+    for (let i = 0; i < customersCount; i++) {
+      let customer: ScanCustomer = {
+        jobsCount: Math.round(randn_bm(0, averageCustomerRequestSize * 2, 1)),
+        arrivalTick: Math.round(Math.random() * simDuration),
+        finishedTick: null,
+      };
+      customers.push(customer);
+    }
+  };
+
+  let customerPointer = 0;
+  const advanceTick = () => {
+    for (let i = 0; i < workers; i++) {
+      // advance the pointer
+      if (customerPointer >= customers.length - 1) {
+        customerPointer = 0;
+      } else {
+        customerPointer++;
+      }
+      if (
+        currentTick > customers[customerPointer].arrivalTick &&
+        customers[customerPointer].finishedTick == null
+      ) {
+        if (customers[customerPointer].jobsCount > 0) {
+          customers[customerPointer].jobsCount--; // finish a scan
+        } else {
+          // if done, set the finishedTick and remove from look
+          customers[customerPointer].finishedTick = currentTick;
+        }
+      }
+      currentTick++;
+    }
+  };
+
+  const run = () => {
+    let start = performance.now();
+    console.log(`Sim duration: ${(simDuration * secsPerTick).toFixed(2)}s`);
+    for (let j = 0; j < simRuns; j++) {
+      // start a new run
+      provisionCustomers();
+      for (let i = 0; i < simDuration; i++) {
+        advanceTick();
+      }
+      console.log(customers);
+      analyseResults();
+    }
+    let delta = performance.now() - start;
+
+    console.log(`Finished, took: ${delta.toFixed(2)}ms`);
+  };
+
+  const analyseResults = () => {
+    // TODO generate report
+    /*
+   - multiply values by secsPerTick to convert to seconds!
+   - calculate average job completion time (in minutes)
+   - generate jobs graph (simple array from sampling?)
+   - generate customer timeline 
+   - push to results array
+  */
+    return null;
+  };
+
+  run();
+  return new Promise((resolve, reject) => {
+    resolve(true);
+  })
+}
+
+// Exponential random number generator
+function randomExponential(lambda: number) {
+    return -Math.log(1 - Math.random()) / lambda;
+  }
+  
+  // Standard Normal variate using Box-Muller transform.
+  function randn_bm(min: number, max: number, skew: number) {
+    let u = 0,
+      v = 0;
+    while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random();
+    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+  
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) num = randn_bm(min, max, skew); // resample between 0 and 1 if out of range
+    num = Math.pow(num, skew); // Skew
+    num *= max - min; // Stretch to fill range
+    num += min; // offset to min
+    return num;
+  }
